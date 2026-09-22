780fac182504d66fca2d102da7e1365ead6bb469
teapo <75266237+4teapo@users.noreply.github.com>
2026-09-22 12:08:11 +0200

    COUNT IT

---
+++ counting/main.py
@@ -0,0 +1,12 @@
+def countWords(words, ignore):
+    wdict = {}
+    ignoredict = set()
+    for wd in ignore:
+        ignoredict.add(wd)
+    for wd in words:
+        if wd in ignoredict:
+            continue
+        wdict[wd] = wdict.get(wd, 0) + 1
+    return wdict
+
+print(countWords(['it','is','a','book', 'book'], ['a','it']))
