d09e8765d02acf9c2b44bef712f4f736e312534e
Noah <smileygamer06@gmail.com>
2026-09-24 12:51:43 +0200

    moved code of topmost.py into a main() function and call it

--- topmost.py
+++ topmost.py
@@ -2,33 +2,34 @@ import sys
 import wordfreq
 import urllib.request
 
-input_stop = sys.argv[1]
-input_content = sys.argv[2]
-words_to_print = int(sys.argv[3])
+def main(input_stop, input_content, words_to_print):
 
 # get stopwords
-stop_words_file = open(input_stop, encoding="utf-8")
+    stop_words_file = open(input_stop, encoding="utf-8")
 
-stop_words = []
-for line in stop_words_file:
-    stop_words.append(line.strip())
+    stop_words = []
+    for line in stop_words_file:
+        stop_words.append(line.strip())
 
-stop_words_file.close()
+    stop_words_file.close()
 
 # tokenize input
-if input_content[:7] == 'http://' or input_content[:8] == 'https://':
-    response = urllib.request.urlopen(input_content)
-    lines = response.read().decode("utf8").splitlines()
-else:
-    content_file = open(input_content, encoding="utf-8")
-    lines = []
-    for line in content_file:
-        lines.append(line.strip())
-    content_file.close()
-content_words = wordfreq.tokenize(lines)
+    if input_content[:7] == 'http://' or input_content[:8] == 'https://':
+        response = urllib.request.urlopen(input_content)
+        lines = response.read().decode("utf8").splitlines()
+    else:
+        content_file = open(input_content, encoding="utf-8")
+        lines = []
+        for line in content_file:
+            lines.append(line.strip())
+        content_file.close()
+    content_words = wordfreq.tokenize(lines)
 
 # count words
-word_count = wordfreq.countWords(content_words, stop_words)
+    word_count = wordfreq.countWords(content_words, stop_words)
 
 # print top words
-wordfreq.printTopMost(word_count, words_to_print)
+    wordfreq.printTopMost(word_count, words_to_print)
+
+
+main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
